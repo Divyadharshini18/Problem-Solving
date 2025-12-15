1a332de62543b91f36adeddabc72c2b6f7ec647b
Divyadharshini18 <divyasadhu725@gmail.com>
2025-12-15 12:32:42 +0530

    9. palindrome Number

---
+++ 9. Palindrome Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        if(x<=9)
+            return true;
+        int rev=0,temp=x;
+        while(temp>0){
+            rev=rev*10+(temp%10);
+            temp/=10;
+        }
+        return rev==x;
+    }
+}
