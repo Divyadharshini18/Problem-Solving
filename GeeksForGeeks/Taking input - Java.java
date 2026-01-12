840b005ab064a14fc9e2d72284d485faadfb565c
Divyadharshini18 <divyasadhu725@gmail.com>
2026-01-12 10:43:12 +0530

    Taking input

---
+++ GeeksForGeeks/Taking input - Java.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        // Take all inputs
+        // Print all the values
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        float b=s.nextFloat();
+        long c=s.nextLong();
+        byte d=s.nextByte();
+        String s1=s.next();
+        String s2=s.next();
+        String s3=s.next();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s1+" "+s2+" "+s3);
+    }
+}
