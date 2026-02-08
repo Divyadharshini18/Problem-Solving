e461452a1b34fb2e92d1d390cd446786635f38ec
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-08 11:35:36 +0530

    class name changed

--- Codeforces/Solution723A.java
+++ Codeforces/Solution723A.java
@@ -1,6 +1,6 @@
 package Codeforces;
 import java.util.*;
-public class Main{
+public class Solution723A{
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int a=s.nextInt();
