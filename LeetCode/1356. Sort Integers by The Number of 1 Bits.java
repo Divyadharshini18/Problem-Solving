28de25460fe63f5be644fe70efd8fad982edb5c8
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-10 11:29:57 +0530

    Sol : Sort int by the no of 1 bits

--- LeetCode/1356. Sort Integers by The Number of 1 Bits.java
+++ LeetCode/1356. Sort Integers by The Number of 1 Bits.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Solution {
+class Solution1 {
     public int[] sortByBits(int[] arr) {
         Integer[] s=Arrays.stream(arr).boxed().toArray(Integer[]::new);
         Arrays.sort(s,(a,b)->{
@@ -18,3 +18,22 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public int[] sortByBits(int[] arr) {
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(a[1]==b[1]?a[0]-b[0]:a[1]-b[1]));
+        for(int n:arr)
+            pq.add(new int[]{n,setBit(n)});
+        for(int i=0;i<arr.length;i++)
+            arr[i]=pq.poll()[0];
+        return arr;
+    }
+    public int setBit(int n){
+        int o=0;
+        while(n>0){
+            if(n%2==1)
+                o++;
+            n/=2;
+        }
+        return o;
+    }
+}
