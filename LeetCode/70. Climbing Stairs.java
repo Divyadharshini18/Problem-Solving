ac9d4a3b033fefa96f73b6285565ac2f98a155f2
Divyadharshini18 <divyasadhu725@gmail.com>
2026-03-19 09:05:54 +0530

    Climbing stairs - space optimisation

--- LeetCode/70. Climbing Stairs.java
+++ LeetCode/70. Climbing Stairs.java
@@ -24,3 +24,19 @@ class Solution1 {
         return noOfWaysToClimb(n,dp);
     }
 }
+
+class Solution2 {
+    public int climbStairs(int n) {
+        if(n<=3)
+            return n;
+        int a=1;
+        int b=2;
+        int c=0;
+        for(int i=3;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
